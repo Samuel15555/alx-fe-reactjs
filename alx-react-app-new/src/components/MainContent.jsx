3f46ad9144ec6dc5ecee267458cde813dd819018
Samuel15555 <sameze888@gmail.com>
2025-11-09 04:06:04 +0000

    Apply inline CSS styling to all React components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f2f2f2', color: '#333', padding: '15px', textAlign: 'center' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
